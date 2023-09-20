dac95fe8cc9373131cfb8208aa900a086cf18f79
Abdela Umer <adheremo65@gmail.com>
2023-09-19 23:25:42 -0400

    RESTful api get and post lab

--- server/app.py
+++ server/app.py
@@ -17,10 +17,39 @@ db.init_app(app)
 api = Api(app)
 
 class Plants(Resource):
-    pass
+    def get(self):
+        all_plants = Plant.query.all()
+        plants_dict = [item.to_dict() for item in all_plants]
+        response = make_response(plants_dict,200)
+        return response
+    def post(self):
+        data = request.get_json()
+        request_body = Plant(
+            name = data.get("name"),
+            image = data.get("image"),
+            price = data.get("price"),
+        )
+        db.session.add(request_body)
+        db.session.commit()
+        response_dict = request_body.to_dict()
+        response = make_response(response_dict,200)
+        return response
+    
+api.add_resource(Plants,"/plants")
 
 class PlantByID(Resource):
-    pass
+    def get(self, id):  # Use the GET method to retrieve a plant by ID
+        single_plant = Plant.query.filter_by(id=id).first()
+        if single_plant:
+            single_dict = single_plant.to_dict()
+            response = make_response(single_dict, 200)
+            return response
+        else:
+            return {"error": "Plant not found"}, 404
+
+api.add_resource(PlantByID, "/plants/<int:id>")
+
+    
         
 
 if __name__ == '__main__':
